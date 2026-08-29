7cfd4956bcda4d3b2af2cadf9c65d964c5dde904
shkn1-marko <shkn1.marko@gmail.com>
2026-08-29 08:32:58 +0200

    chore: add 'Pong' project, add Go + AI/ML as goals

--- data.py
+++ data.py
@@ -8,35 +8,32 @@ db.add_all(
     [
         Entry(
             type=EntryType.right_now,
-            title="Izrađujem web-stranicu koristeći FastAPI",
-            description="Za izradu stranice koristim Python, FastAPI, Jinja. Stranicu poslužujem na domeni marko-palameta.com.hr uz pomoć VPS, Nginx, Systemd.",
-            github_url = "https://github.com/shkn1-marko/project-tracker",
+            title="Izrađujem igru 'Pong' pomoću OpenGL-a",
+            description="Izrađujem osnovno sučelje za igru Pong, koristim C++ i OpenGL.",
+            github_url="https://github.com/shkn1-marko/pong",
         ),
-        Entry(type=EntryType.goal,
-              title="PyTorch / Tensorflow",
-              description="Želim saznati više o ML / AI bibliotekama izradom vlastitih projekata",
+        Entry(
+            type=EntryType.goal,
+            title="Go",
+            description="Želim ponovo raditi u Go programskom jeziku.",
         ),
-        Entry(type=EntryType.goal,
-              title="Docker / Kubernetes",
-              description="Želim saznati što je to Docker / Kubernetes",
+        Entry(
+            type=EntryType.goal,
+            title="AI/ML",
+            description="Želim dodati u svoj portfolio jedan projekt koji koristi AI/ML.",
+        ),
+        Entry(
+            type=EntryType.project,
+            title="Pong",
+            description="U slobodno vrijeme izrađujem video igre. Pong je jednostavna igra pomoću koje želim naučiti više o programiranju u C++-u i OpenGL-u. Želim naučiti kako izrađivati veće sustave u C++-u.",
+            github_url="https://github.com/shkn1-marko/pong",
+            status=Status.yellow,
         ),
         Entry(
             type=EntryType.project,
             title="Web-stranica za praćenje projekata",
             description="Prilikom prijave za poslove suočio sam se sa jedinstvenim problemom. Kako potencijalnom poslodavcu najbolje prestaviti svoj portfolio. Došao sam na ideju da stvorim jedinstveni pogled koji spaja podatke koji se inače nalaze u mom životopisu, na GitHub-u i na web-stranicama mojih projekata. Podatci su podjeljeni u tri kategorije, stvari na kojima upravo radim, dugoročni ciljevi i popis završenih (zeleno) i projekata koji su u izradi (žuto).",
             github_url="https://github.com/shkn1-marko/project-tracker",
-            status=Status.green,
-        ),
-        Entry(
-            type=EntryType.project,
-            title="Pokretač video igara",
-            description="U slobodno vrijeme izrađujem vlastiti pokretač video igara (C++).",
-            status=Status.yellow,
-        ),
-        Entry(
-            type=EntryType.project,
-            title="Video igra (Online/Multiplayer/Server-Client)",
-            description="Izradio sam igru (C++) za dva igrača. Napisao sam server (Go) koji omogućuje igračima da razmjenjuju poteze putem interneta.",
             status=Status.green,
         ),
     ]
